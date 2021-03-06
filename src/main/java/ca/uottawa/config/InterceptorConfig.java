bae69a8836b4aef91366247f085c37ad6d0e7516
YukangYin <yyin050@uottawa.ca>
2021-03-06 17:55:47 -0500

    [CODE] add global authentication interceptor

---
+++ src/main/java/ca/uottawa/config/InterceptorConfig.java
@@ -0,0 +1,18 @@
+package ca.uottawa.config;
+
+import ca.uottawa.interceptor.JWTInterceptor;
+import com.auth0.jwt.JWT;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // all apis except login need authentication
+        registry.addInterceptor(new JWTInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/api/login");
+    }
+}
---
+++ src/main/java/ca/uottawa/interceptor/JWTInterceptor.java
@@ -0,0 +1,32 @@
+package ca.uottawa.interceptor;
+
+import ca.uottawa.utils.JWTUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JWTInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("auth-token");  // get jwt token from request header
+
+        try {
+            JWTUtil.verifyToken(token);
+            return true;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "Please login first");
+        String json = new ObjectMapper().writeValueAsString(map);
+        response.setStatus(401);  // unauthorized
+        response.setContentType("application/json;character=UTF-8");
+        response.getWriter().println(json);
+        return false;
+    }
+}
